218fac3777ac38b524dcbae35681ef0b142bb1ba
ailtonbsj <ailton.ifce@gmail.com>
2018-07-28 23:18:41 -0300

    bug on backgroun color

---
+++ headerBar.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk, Gio
+
+class HeaderBarWindow(Gtk.Window):
+
+    def __init__(self):
+        Gtk.Window.__init__(self, title="HeaderBar Demo")
+        self.set_border_width(10)
+        self.set_default_size(400, 200)
+
+        hb = Gtk.HeaderBar()
+        hb.set_show_close_button(True)
+        hb.props.title = "HeaderBar example"
+        self.set_titlebar(hb)
+
+        button = Gtk.Button()
+        #Icons are available in /usr/share/icons/gnome/32x32/actions
+        icon = Gio.ThemedIcon(name="gtk-paste")
+        image = Gtk.Image.new_from_gicon(icon, Gtk.IconSize.BUTTON)
+        button.add(image)
+        hb.pack_end(button)
+        
+        box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
+        Gtk.StyleContext.add_class(box.get_style_context(), "linked")
+
+        button = Gtk.Button()
+        button.add(Gtk.Arrow(Gtk.ArrowType.LEFT, Gtk.ShadowType.NONE))
+        box.add(button)
+
+        button = Gtk.Button()
+        button.add(Gtk.Arrow(Gtk.ArrowType.RIGHT, Gtk.ShadowType.NONE))
+        box.add(button)
+
+        hb.pack_start(box)
+        
+        self.add(Gtk.TextView())
+
+win = HeaderBarWindow()
+win.connect("destroy", Gtk.main_quit)
+win.show_all()
+Gtk.main()
